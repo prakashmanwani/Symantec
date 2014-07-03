d1d060da52c04f4e5542807e23fd2577a319e069
Prakash <thirdpen@gmail.com>
2014-07-03 02:07:42 -0700

    Recent commit of Hibernate integration with userdetail and static save

--- src/main/java/com/symantec/dao/UserDaoImpl.java
+++ src/main/java/com/symantec/dao/UserDaoImpl.java
@@ -1,22 +1,19 @@
 package com.symantec.dao;
 
-import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import com.symantec.entity.Health;
 import com.symantec.entity.UserDetail;
 
 @Repository
-public class UserDaoImpl implements UserDao {
-	private SessionFactory sessionFactory;
-	
+public class UserDaoImpl extends HibernateDaoSupport implements UserDao {
+
 	public void addUser(UserDetail usd) {
-	    this.sessionFactory.getCurrentSession().save(usd);
-		
+	    getHibernateTemplate().save(usd);
 	}
-	
 	public void addHealth(Health hsrc){
-		this.sessionFactory.getCurrentSession().save(hsrc);
+		getHibernateTemplate().save(hsrc);
 	}
 
 }
